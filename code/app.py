379e9066b02c84de58d971acb2a5f7dabbb394e2
binarysaurus <im@joe.cloud>
2017-01-16 20:17:33 -0600

    sample motor test

--- code/app.py
+++ code/app.py
@@ -7,7 +7,7 @@ GPIO.setwarnings(False)
 
 motor1f = 8
 motor1b = 7
- 
+
 GPIO.setup(motor1f, GPIO.OUT)
 GPIO.setup(motor1b, GPIO.OUT)
 
@@ -16,7 +16,7 @@ GPIO.setup(motor1b, GPIO.OUT)
 
 def main():
     val = input('arg: ')
-    while (val != 'q'):
+    while (True):
         GPIO.output(motor1f, bool(literal_eval(val)))
         GPIO.output(motor1b, not bool(literal_eval(val)))
         val = input('arg: ')
