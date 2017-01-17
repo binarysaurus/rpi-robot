aa8bd683a093a86cc4069e65748c0d524919e7b6
binarysaurus <im@joe.cloud>
2017-01-16 19:47:59 -0600

    sample motor test

--- code/app.py
+++ code/app.py
@@ -2,6 +2,16 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
+GPIO.setwarnings(False)
+val = input('arg: ')
 
-GPIO.output(21, 0)
+while (val != 'q'):
+    GPIO.output(21, 1 if str2bool(val) else 0)
+    GPIO.output(20, 0 if str2bool(val) else 1)
+    val = input('arg: ')
 #GPIO.output(20, 1)
+
+
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
