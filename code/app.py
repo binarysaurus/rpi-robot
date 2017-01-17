0b04006eb9213f06cdd9aa3a332d290dc36e8872
binarysaurus <im@joe.cloud>
2017-01-16 20:11:54 -0600

    sample motor test

--- code/app.py
+++ code/app.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -15,12 +16,14 @@ GPIO.setup(motor1b, GPIO.OUT)
 def main():
     val = input('arg: ')
     while (val != 'q'):
-        #GPIO.output(motor1f, bool(literal_eval(val)))
-        #GPIO.output(motor1b, not bool(literal_eval(val)))
+        GPIO.output(motor1f, bool(literal_eval(val)))
+        GPIO.output(motor1b, not bool(literal_eval(val)))
         val = input('arg: ')
 
-GPIO.output(motor1f, 0)
+GPIO.output(motor1f, 1)
 GPIO.output(motor1b, 0)
+time.sleep(10)
+GPIO.output(motor1f, 0)
 GPIO.cleanup()
 
 
