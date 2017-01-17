a92b481081506878fb81224e1b0e9cfa258adc24
binarysaurus <im@joe.cloud>
2017-01-16 20:05:13 -0600

    sample motor test

--- code/app.py
+++ code/app.py
@@ -1,9 +1,10 @@
 from ast import literal_eval
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
-GPIO.setwarnings(False)
+
 
 
 
@@ -14,6 +15,10 @@ def main():
         GPIO.output(20, not bool(literal_eval(val)))
         val = input('arg: ')
     #GPIO.output(20, 1)
+
+GPIO.output(21, 0)
+GPIO.output(20, 0)
+GPIO.cleanup()
 
 
 if __name__ == "__main__":
