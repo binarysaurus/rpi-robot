5bce7f4c5af3343aebf5c003128fde45ae297472
binarysaurus <im@joe.cloud>
2017-03-15 20:40:23 -0500

    testing 4 motor

--- code/control.py
+++ code/control.py
@@ -2,11 +2,19 @@ import move as mv
 import time
 
 FL = mv.motor(2, 3, 4, 1)
-#FR = 
-#BL = 
-#BR = 
+FR = mv.motor(5, 6, 13, -1)
+BL = mv.motor(10, 9, 11, 1)
+BR = mv.motor(17, 27, 22, -1)
+
+
+def moveall(FL, FR, BL, BR, speed, dir):
+	FL.move(speed, 1)
+	BL.move(speed, 1)
+	FR.move(speed, 1)
+	BR.move(speed, 1)
+
 for i in range(100):
-	FL.move(i, 1)
+	moveall(FL, FR, BL, BR, i, 1)
 	time.sleep(0.7)
-	
+
 FL.halt(exitstop=True)
