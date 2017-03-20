f67d84af932a1bd8cc79e3fd72eb78cde8e1b5fc
binarysaurus <im@joe.cloud>
2017-03-20 00:20:09 -0500

    fix motion.py typo

--- code/motion.py
+++ code/motion.py
@@ -29,7 +29,7 @@ class Motor(object):
     	GPIO.output(self.forwardpin, GPIO.LOW)
     	GPIO.output(self.backwardpin, GPIO.LOW)
 
-def drive(leftmotors, rightmotors speed, dir, turn = 0):
+def drive(leftmotors, rightmotors, speed, dir, turn = 0):
     assert abs(speed - turn) <= 100, "Duty cycle cannot exceed 100%!"
     assert abs(speed + turn) <= 100, "Duty cycle cannot exceed 100%!"
     left.move(abs(speed + turn), dir) for left in leftmotors
