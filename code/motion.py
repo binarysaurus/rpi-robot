25fbdc16952ab76ece6f8a031d0c00caca9751a1
binarysaurus <im@joe.cloud>
2017-03-19 23:52:05 -0500

    changed L & R tank drive

--- code/motion.py
+++ code/motion.py
@@ -34,8 +34,8 @@ def drive(fl_motor, fr_motor, bl_motor, br_motor, speed, dir, turn = 0):
     assert abs(speed - turn) <= 100, "Duty cycle cannot exceed 100%!"
     assert abs(speed + turn) <= 100, "Duty cycle cannot exceed 100%!"
     fl_motor.move(abs(speed + turn), dir)
-    fr_motor.move(abs(speed + turn), dir)
-    bl_motor.move(abs(speed - turn), dir)
+    fr_motor.move(abs(speed - turn), dir)
+    bl_motor.move(abs(speed + turn), dir)
     br_motor.move(abs(speed - turn), dir)
 
 def stopdrive(fl_motor, fr_motor, bl_motor, br_motor):
