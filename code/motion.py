9491d839b0c43c7046bb705058e78a365a2395a2
binarysaurus <im@joe.cloud>
2017-03-20 00:03:36 -0500

    remove redundant assert

--- code/motion.py
+++ code/motion.py
@@ -31,7 +31,6 @@ class Motor(object):
     	GPIO.output(self.backwardpin, GPIO.LOW)
 
 def drive(fl_motor, fr_motor, bl_motor, br_motor, speed, dir, turn = 0):
-    assert abs(speed - turn) <= 100, "Duty cycle cannot exceed 100%!"
     assert abs(speed + turn) <= 100, "Duty cycle cannot exceed 100%!"
     fl_motor.move(abs(speed + turn), dir)
     fr_motor.move(abs(speed - turn), dir)
