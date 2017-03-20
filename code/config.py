86a4ccb7354ac0f1d5e9374923ecd0ae0de3e6dd
binarysaurus <im@joe.cloud>
2017-03-19 23:49:59 -0500

    included back right motor

--- code/config.py
+++ code/config.py
@@ -17,12 +17,12 @@ bl_bp = 9
 bl_sp = 11
 
 # Back Right motor GPIO pins
-bl_fp = 17
-bl_bp = 27
-bl_sp = 22
+br_fp = 17
+br_bp = 27
+br_sp = 22
 
 # Motor pins set
 fl_motor = motion.Motor(fl_fp, fl_bp, fl_sp, False)
 fr_motor = motion.Motor(fr_fp, fr_bp, fr_sp, True)
 bl_motor = motion.Motor(bl_fp, bl_bp, bl_sp, False)
-bl_motor = motion.Motor(bl_fp, bl_bp, bl_sp, True)
+br_motor = motion.Motor(br_fp, br_bp, br_sp, True)
